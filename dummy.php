00b2e2c8debdb2953594cfe0912033f6f3a1c9c3
fun-withcode <dhanalakshmikamal2003@gmail.com>
2024-11-02 13:06:52 +0530

    dummy.php file added

---
+++ dummy.php
@@ -0,0 +1,15 @@
+<?php
+// Simple PHP program to add two numbers
+
+// Function to add two numbers
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+
+// Example usage
+$num1 = 10;
+$num2 = 20;
+$sum = addNumbers($num1, $num2);
+
+echo "The sum of $num1 and $num2 is: $sum";
+?>
